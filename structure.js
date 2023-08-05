1c8add995cf332a0f7980fb98b4002321a931f10
UniBreakfast <unibreakfast1@gmail.com>
2023-08-05 21:18:47 +0300

    add createStructure and getStructure functions

--- structure.js
+++ structure.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const { resolve, sep } = require('path')
+
 /* 
 function takes an object (tree) describing a directory structure like this:
 {
@@ -35,12 +38,13 @@ and returns a string representing the structure like this:
   ├─📁subfolder3
   └─📄file5
 */
+
 {
   const tree1 = {
     name: 'file',
   }
   const struct1 = structure(tree1)
-  console.log(struct1, '\n');
+  console.log(struct1, '\n')
   // └─📄file
 }
 
@@ -50,7 +54,7 @@ and returns a string representing the structure like this:
     children: [],
   }
   const struct2 = structure(tree2)
-  console.log(struct2, '\n');
+  console.log(struct2, '\n')
   // └─📁folder
 }
 
@@ -62,7 +66,7 @@ and returns a string representing the structure like this:
     ],
   }
   const struct3 = structure(tree3)
-  console.log(struct3, '\n');
+  console.log(struct3, '\n')
   // └─📁folder
   //   └─📄file
 }
@@ -81,7 +85,7 @@ and returns a string representing the structure like this:
     ],
   }
   const struct4 = structure(tree4)
-  console.log(struct4, '\n');
+  console.log(struct4, '\n')
   // └─📁folder
   //   └─📁subfolder1
   //     ├─📄file1
@@ -105,7 +109,7 @@ and returns a string representing the structure like this:
     ],
   }
   const struct5 = structure(tree5)
-  console.log(struct5, '\n');
+  console.log(struct5, '\n')
   // └─📁folder
   //   ├─📁subfolder1
   //   │ └─📄file1
@@ -135,9 +139,9 @@ and returns a string representing the structure like this:
       { name: 'file2' },
     ],
   }
-  
+
   const struct6 = structure(tree6)
-  console.log(struct6);
+  console.log(struct6, '\n')
   // ├─📁folder
   // │ └─📁subfolder1
   // │   └─📁subfolder2
@@ -150,14 +154,93 @@ function structure(tree, last = true, depth = [0]) {
   const icon = tree.children ? '📁' : '📄'
   const line = last ? '└' : '├'
 
-  let result = `${line}─${icon}${tree.name}`;
-  
+  let result = `${line}─${icon}${tree.name}`
+
   if (tree.children) {
     tree.children.forEach((child, i, arr) => {
       const last = i === arr.length - 1
-      const indent = depth.map(line => line ? '│ ' : '  ').join('');
+      const indent = depth.map(line => line ? '│  ' : '   ').join('')
       result += '\n' + indent + structure(child, last, [...depth, !last])
     })
   }
   return result
+}
+
+/* 
+Create folders and empty files according to the structure
+*/
+
+function createStructure(tree, path = '.') {
+  const { name, children } = tree
+  const dir = path + '/' + name
+  if (children) {
+    try {
+      fs.mkdirSync(dir)
+    } catch { }
+    children.forEach(child => {
+      try {
+        createStructure(child, dir)
+      } catch { }
+    })
+  }
+  else {
+    try {
+      fs.writeFileSync(dir, '')
+    } catch { }
+  }
+}
+
+{
+  const tree6 = {
+    name: 'folder',
+    children: [
+      {
+        name: 'subfolder1',
+        children: [
+          {
+            name: 'subfolder2',
+            children: [
+              {
+                name: 'subfolder3',
+                children: [
+                  { name: 'file1' },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      { name: 'file2' },
+    ],
+  }
+  createStructure(tree6)
+}
+
+/*
+Create a structure from a directory
+*/
+
+function getStructure(path = '.', ignore = []) {
+  path = resolve(path)
+  const name = path.split(sep).pop()
+  const isFolder = fs.lstatSync(path).isDirectory()
+  const tree = { name }
+  if (isFolder) {
+    tree.children = fs.readdirSync(path)
+      .filter(child => !ignore.includes(child))
+      .map(child => getStructure(path + sep + child))
+      .sort((a, b) => a.children && !b.children ? -1 : !a.children && b.children ? 1 : 0)
+  }
+  return tree
+}
+
+{
+  const struct = structure(getStructure())
+  console.log(struct, '\n')
+}
+
+{
+  const ignore = ['node_modules', '.git']
+  const struct = structure(getStructure('.', ignore))
+  console.log(struct, '\n')
 }
